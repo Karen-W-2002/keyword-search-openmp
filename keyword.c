f03f6a4ad8aade1493eb789ffc9c27cd04820d6e
Karen W <karenweidev@gmail.com>
2021-12-24 01:46:54 +0800

    Update Queue Functions

--- keyword.c
+++ keyword.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <omp.h>
 
 #define BUFSIZE 1024
-
-void Get_File(char *argv[], FILE *fp[], int file_count);
-void Read_File(FILE *fp[], char buf[][BUFSIZE], int file_count);
-void Close_File(FILE *fp[], int file_count);
-void producer_consumer_func(int producer_count, int consumer_count, FILE *fp[], int file_count);
-struct Queue *create_queue();
-struct QNode *new_node(char *buf);
-void enqueue(struct Queue *q, char *buf);
-void dequeue(struct Queue *q);
 
 struct QNode {
     char *buf;
@@ -22,17 +14,28 @@ struct Queue {
     struct QNode *head, *rear;
 };
 
+void Get_File(char *argv[], FILE *fp[], int file_count);
+void Read_File(struct Queue *q, FILE *fp);
+void Close_File(FILE *fp[], int file_count);
+void producer_consumer_func(int producer_count, int consumer_count, FILE *fp[], int file_count);
+struct Queue *create_queue();
+struct QNode *new_node(char *buf);
+void enqueue(struct Queue *q, char *buf);
+void dequeue(struct Queue *q);
+void print_queue(struct Queue *q);
+
 int main(int argc, char *argv[])
 {
+    //struct Queue *q = create_queue();
     int producer_count = strtol(argv[1], NULL, 10);
     int consumer_count = strtol(argv[2], NULL, 10);
     int file_count = strtol(argv[3], NULL, 10);
     FILE *fp[file_count];
-    char buffer[file_count][BUFSIZE];
  
-    Get_File(argv, fp, file_count); 
-    //producer_consumer_func(producer_count, consumer_count);
-
+    Get_File(argv, fp, file_count);
+    //Read_File(q, fp[0], file_count);
+    producer_consumer_func(producer_count, consumer_count, fp, file_count);
+    //print_queue(q);
     Close_File(fp, file_count);
     return 0;
 }
@@ -52,14 +55,18 @@ void Get_File(char *argv[], FILE *fp[], int file_count)
     }
 }
 
-void Read_File(FILE *fp[], char buf[][BUFSIZE], int file_count)
+void Read_File(struct Queue *q, FILE *fp)
 {
-    int i;
-
-    for(i = 0; i < file_count; i++)
+    char *line = malloc(BUFSIZE * sizeof(char));
+    
+    while(fgets(line, BUFSIZE, fp) != NULL)
     {
-        fread(&(buf[i]), 1024, 1, fp[i]);
+        line[strcspn(line, "\n")] = 0;
+        enqueue(q, line);
+        line = malloc(BUFSIZE * sizeof(char));
     }
+
+    // maybe close file can be here instead of a function
 }
 
 void Close_File(FILE *fp[], int file_count)
@@ -74,8 +81,30 @@ void Close_File(FILE *fp[], int file_count)
 
 void producer_consumer_func(int producer_count, int consumer_count, FILE *fp[], int file_count)
 {
+    struct Queue *key_q = create_queue();
     int thread_count = producer_count + consumer_count;
-    
+    int prod_done_count = 0;
+
+#   pragma omp parallel num_threads(thread_count) default(none) shared(file_count, key_q, fp, producer_count, consumer_count, prod_done_count)
+    {
+        int i = 0;
+        int my_rank = omp_get_thread_num();
+        
+        if(my_rank < producer_count) // Producer Threads
+        {
+            for(i = my_rank; i < file_count; i += producer_count)
+            {
+                // writing to the q is critical tho?
+                Read_File(key_q, fp[i]);
+            }
+#           pragma omp atomic
+            prod_done_count++;
+        }
+        else // Consumer Threads
+        {
+           while(prod_done_count < producer_count); 
+        }
+    }
 }
 
 /*
@@ -102,7 +131,7 @@ void enqueue(struct Queue *q, char *buf)
     struct QNode *temp = new_node(buf);
     if(q->rear == NULL)
     {
-        q->head = q->rear = NULL;
+        q->head = q->rear = temp;
         return;
     }
 
@@ -123,4 +152,30 @@ void dequeue(struct Queue *q)
         q->rear = NULL;
 
     free(temp);
+}
+
+void print_queue(struct Queue *q)
+{
+    struct QNode *temp = q->head;
+
+    if(temp == NULL)
+    {
+        printf("NULL\n");
+        return;
+    }
+
+    if(temp->next != NULL)
+    {
+        while(temp)
+        {
+            printf("%s -> ", temp->buf);
+            temp = temp->next;
+        }
+    }
+    else
+    {
+        printf("%s -> ", temp->buf);
+    }
+
+    printf("NULL\n");
 }
