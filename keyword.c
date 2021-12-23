cfec0b4e4924bf8303a9b99ca84af50032853397
Karen W <karenweidev@gmail.com>
2021-12-23 19:58:06 +0800

    Add read files function

--- keyword.c
+++ keyword.c
@@ -2,22 +2,70 @@
 #include <stdlib.h>
 #include <omp.h>
 
-void Hello(void);
+#define BUFSIZE 1024
+
+void read_files(char *argv[], char buf[][BUFSIZE], int file_count);
+void producer_consumer_func(int producer_count, int consumer_count);
+
+
 
 int main(int argc, char *argv[])
 {
-    int thread_count = strtol(argv[1], NULL, 10);
-# pragma omp parallel num_threads(thread_count)
+    //int i;
+    int producer_count = strtol(argv[1], NULL, 10);
+    int consumer_count = strtol(argv[2], NULL, 10);
+    int file_count = strtol(argv[3], NULL, 10);
+    //FILE *fp[file_count];
+    char buffer[file_count][BUFSIZE];
 
-    Hello();
+    // Read file
+    /*for(i = 0; i < file_count; i++)
+    {
+        fp[i] = fopen(argv[i + 4], "r");
+
+        if(fp[i] == NULL)
+        {
+            printf("Error on file open!\n");
+            exit(0);
+        }
+    }*/
+
+    //Get_files(fp, &file_count);
+    //fread(&(buffer[0]), 1024, 1, fp[0]);
+    read_files(argv, buffer, file_count);
+    printf("%s\n", buffer[1]);
+    producer_consumer_func(producer_count, consumer_count);
+
+    /*for(i = 0; i < file_count; i++)
+    {
+        fclose(fp[i]);
+    }*/
 
     return 0;
 }
 
-void Hello(void)
+void read_files(char *argv[], char buf[][BUFSIZE], int file_count)
 {
-    int my_rank = omp_get_thread_num();
-    int thread_count = omp_get_num_threads();
+    FILE *fp[file_count];
+    int i;
 
-    printf("Hello from thread %d of %d\n", my_rank, thread_count);
+    for(i = 0; i < file_count; i++)
+    {
+        fp[i] = fopen(argv[i+4], "r");
+
+        if(fp[i] == NULL)
+        {
+            printf("Error on read!\n");
+            exit(0);
+        }
+    
+        fread(&(buf[i]), 1024, 1, fp[i]);
+        fclose(fp[i]);
+    }
+}
+
+void producer_consumer_func(int producer_count, int consumer_count)
+{
+    //int thread_count = producer_count + consumer_count;
+
 }
